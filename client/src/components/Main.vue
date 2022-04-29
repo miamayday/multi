f0f154decc79fc2058ee0c54fe10546c66569b1e
Mia Mantila <mia.mantila@aalto.fi>
2022-04-29 22:51:56 +0300

    Add Main page

---
+++ client/src/components/Main.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+import type Message from '../types/Message'
+import Chat from './Chat.vue'
+import Profile from './Profile.vue'
+
+interface Props {
+  // Profile
+  username: string
+  changeUsername: (username: string) => void
+  // Chat
+  messages: Message[]
+  sendMessage: (content: string) => void
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div id="main">
+    <div class="wrapper">
+      <Profile :username="username" :changeUsername="changeUsername" />
+      <Chat :messages="messages" :sendMessage="sendMessage" />
+    </div>
+  </div>
+</template>
+
+<style>
+#main {
+  max-width: 1280px;
+  height: 100vh;
+  padding: 2rem;
+}
+
+#main .wrapper {
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  height: 100%;
+}
+
+#main #profile {
+  margin-bottom: 2rem;
+}
+
+@media (min-width: 1024px) {
+  #main .wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  #main #profile {
+    margin-right: 4rem;
+  }
+
+  #main #chat {
+    width: 500px;
+  }
+}
+</style>
